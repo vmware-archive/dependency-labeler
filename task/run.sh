3446f373b542b148d9c56f32f3e83c26f783fbda
Katie Chapman <kchapman@pivotal.io>
2019-08-30 10:58:24 +0100

    Add helpful error messages when task isn't given required parameters
    [#168055037]
    
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- task/run.sh
+++ task/run.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 source /opt/resource/common.sh
 
-start_docker 3 3 "" ""
+check_env_var_exists() {
+    if [[ -z "${2}" ]]; then
+        echo "You must set the param ${1}"
+        exit 1
+    fi
+}
 
-/deplab --image-tar ${IMAGE_TAR} \
- --git ${GIT_REPO} \
- --output-tar ${OUTPUT_DIR}/image.tar
+main() {
+    check_env_var_exists "IMAGE_TAR" "${IMAGE_TAR}"
+    check_env_var_exists "OUTPUT_DIR" "${OUTPUT_DIR}"
+    check_env_var_exists "GIT_REPO" "${GIT_REPO}"
+
+    start_docker 3 3 "" ""
+
+    /deplab --image-tar ${IMAGE_TAR} \
+     --git ${GIT_REPO} \
+     --output-tar ${OUTPUT_DIR}/image.tar
+}
+
+main
