71dfe7ba5deebf8ee237d63fc4c16d89cf820c72
Iain Sproat <isproat@pivotal.io>
2019-11-14 11:24:20 +0000

    Refactored inspect integration tests to use DescribeTable
    * enable future testing of different image types with similar test logic
    
    [#169699978]
    
    Signed-off-by: Velda Conaty <vconaty@pivotal.io>

--- integration/inspect_integration_test.go
+++ integration/inspect_integration_test.go
@@ -2,85 +2,80 @@ package integration_test
 
 import (
 	"encoding/json"
+	"log"
 	"path/filepath"
 
 	"github.com/pivotal/deplab/metadata"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("deplab inspect", func() {
-	Context("with a deplab-d image tar path", func() {
-		BeforeEach(func() {
+	DescribeTable("prints the label", func(flag, path string) {
+		stdOut, _ := runDepLab([]string{
+			"inspect",
+			flag, path,
+		}, 0)
 
-		})
+		md := metadata.Metadata{}
+		err := json.NewDecoder(stdOut).Decode(&md)
 
-		It("prints the label", func() {
-			inputTarPath := filepath.Join("assets", "tiny-deplabd.tgz")
-			inputTarPath, err := filepath.Abs(inputTarPath)
-			Expect(err).ToNot(HaveOccurred())
-			stdOut, _ := runDepLab([]string{
-				"inspect",
-				"--image-tar", inputTarPath,
-			}, 0)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(md.Provenance[0].Name).To(Equal("deplab"))
+	},
+		Entry("with a deplab'd image tarball", "--image-tar", getTestAssetPath("tiny-deplabd.tgz")),
+	)
 
-			md := metadata.Metadata{}
-			err = json.NewDecoder(stdOut).Decode(&md)
+	DescribeTable("provides an error", func(flag, path string) {
+		_, stderr := runDepLab([]string{
+			"inspect",
+			flag, path,
+		}, 1)
 
-			Expect(err).ToNot(HaveOccurred())
+		Expect(getContentsOfReader(stderr)).To(
+			SatisfyAll(
+				ContainSubstring("deplab cannot find the 'io.pivotal.metadata' label on the provided image"),
+				ContainSubstring("tiny.tgz")))
+	},
+		Entry("with a undeplab'd image tar path", "--image-tar", getTestAssetPath("tiny.tgz")),
+	)
 
-			Expect(md.Provenance[0].Name).To(Equal("deplab"))
-		})
-	})
-	Context("with a undeplabbed image tar path", func() {
-		It("provides an error", func() {
-			inputTarPath := filepath.Join("assets", "tiny.tgz")
-			inputTarPath, err := filepath.Abs(inputTarPath)
-			Expect(err).ToNot(HaveOccurred())
-			_, stderr := runDepLab([]string{
-				"inspect",
-				"--image-tar", inputTarPath,
-			}, 1)
+	DescribeTable("provides an error", func(flag, path string) {
+		_, stderr := runDepLab([]string{
+			"inspect",
+			flag, path,
+		}, 1)
 
-			Expect(getContentsOfReader(stderr)).To(
-				SatisfyAll(
-					ContainSubstring("deplab cannot find the 'io.pivotal.metadata' label on the provided image"),
-					ContainSubstring("tiny.tgz")))
-		})
-	})
+		Expect(getContentsOfReader(stderr)).To(
+			SatisfyAll(
+				ContainSubstring("deplab cannot open the provided image"),
+				ContainSubstring("invalid-image-archive.tgz")))
+	},
+		Entry("with a invalid image tarball", "--image-tar", getTestAssetPath("invalid-image-archive.tgz")),
+	)
 
-	Context("with a invalid image", func() {
-		It("provides an error", func() {
-			inputTarPath := filepath.Join("assets", "invalid-image-archive.tgz")
-			inputTarPath, err := filepath.Abs(inputTarPath)
-			Expect(err).ToNot(HaveOccurred())
-			_, stderr := runDepLab([]string{
-				"inspect",
-				"--image-tar", inputTarPath,
-			}, 1)
+	DescribeTable("provides an error", func(flag, path string) {
+		_, stderr := runDepLab([]string{
+			"inspect",
+			flag, path,
+		}, 1)
 
-			Expect(getContentsOfReader(stderr)).To(
-				SatisfyAll(
-					ContainSubstring("deplab cannot open the provided image"),
-					ContainSubstring("invalid-image-archive.tgz")))
-		})
-	})
-
-	Context("with a valid image but invalid json label", func() {
-		It("provides an error", func() {
-			inputTarPath := filepath.Join("assets", "tiny-with-invalid-label.tgz")
-			inputTarPath, err := filepath.Abs(inputTarPath)
-			Expect(err).ToNot(HaveOccurred())
-			_, stderr := runDepLab([]string{
-				"inspect",
-				"--image-tar", inputTarPath,
-			}, 1)
-
-			Expect(getContentsOfReader(stderr)).To(
-				SatisfyAll(
-					ContainSubstring("deplab cannot parse the label"),
-					ContainSubstring("tiny-with-invalid-label.tgz")))
-		})
-	})
+		Expect(getContentsOfReader(stderr)).To(
+			SatisfyAll(
+				ContainSubstring("deplab cannot parse the label"),
+				ContainSubstring("tiny-with-invalid-label.tgz")))
+	},
+		Entry("with a valid image but invalid json label", "--image-tar", getTestAssetPath("tiny-with-invalid-label.tgz")),
+	)
 })
+
+func getTestAssetPath(path string) string {
+	inputTarPath := filepath.Join("assets", path)
+	inputTarPath, err := filepath.Abs(inputTarPath)
+	if err != nil {
+		log.Fatalf("Could not find test asset %s: %s", path, err)
+	}
+	return inputTarPath
+}
